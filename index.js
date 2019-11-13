519f231a68698c3418ed3a5918f9ba84636b811d
LISA_COOK <lisabpink@gmail.com>
2019-11-13 18:16:32 -0500

    Completed MVP Lisa Cook2

--- index.js
+++ index.js
@@ -241,7 +241,7 @@ function getRunnersByTShirtSize(runners, size){
   return runnersByTShirtSize
 }
 // function getRunnersByTShirtSize(runners, tShirtSize) {
-//   return runners.filter(shirt => shirt.shirt_size === tShirtSize);
+//   return runners.filter(shirt => shirt.shirt_size === tShirtSize);countMakerWith
 // }
 // console.log(getRunnersByTShirtSize)
 
@@ -320,8 +320,14 @@ const newCounter = counterMaker();
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+function counterMakerWithLimit(maxValue) {
+  let count = -1;
+  return function() {
+    if (count === maxValue) {
+      count = -1;
+    }
+    return ++count;
+  };
 }
 
 /////////////// END OF CHALLENGE ///////////////
